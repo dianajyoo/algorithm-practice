26026323aa77f2f03c7a8f1c58f05fb41665fb0c
dianajyoo <diana.yooj@gmail.com>
2019-02-04 15:09:16 -0500

    added algo

---
+++ codewars/6kyu-sqInRect.js
@@ -0,0 +1,30 @@
+<a href="https://www.codewars.com/kata/rectangle-into-squares/train/javascript">Link to Algorithm</a>
+
+function sqInRect(lng, wdth) {
+  let result = [];
+
+  if (lng === wdth) {
+    return null;
+  } else if (lng > wdth) {
+    return checkArea(lng, wdth);
+  } else if (lng < wdth) {
+    return checkArea(wdth, lng);
+  }
+
+  function checkArea(l, w) {
+    result.push(w);
+
+    if ((l - w) % w === 0 && l === w) {
+      return;
+    } else {
+      l -= w;
+      if (l < w) {
+      checkArea(w, l);
+      } else {
+      checkArea(l, w);
+      }
+    }
+
+    return result;
+  }
+}
---
+++ leetcode/m-maxIncreaseKeepingSkyline.js
@@ -0,0 +1,43 @@
+<a href="https://leetcode.com/problems/max-increase-to-keep-city-skyline/">Algorithm #807</a>
+
+var maxIncreaseKeepingSkyline = function(grid) {
+    let topBottomSkyline = [],
+      counter = 0,
+
+      // return max heights of each row
+      leftRightSkyline = grid.reduce( (skyline, heights) => {
+        skyline.push(Math.max(...heights));
+        return skyline;
+      }, []);
+
+    // return max heights of each column
+    for (let i = 0; i < grid[0].length; i++) {
+      let maxHeight = 0;
+      for (let j = 0; j < grid.length; j++) {
+        if (maxHeight <= grid[j][i]) {
+            maxHeight = grid[j][i];
+        }
+      }
+      topBottomSkyline.push(maxHeight);
+    }
+
+    for (let row = 0; row < grid.length; row++) {
+      for (let col = 0; col < grid.length; col++) {
+        if (grid[row][col] <= leftRightSkyline[row] && grid[row][col] <= topBottomSkyline[col]
+          && leftRightSkyline[row] < topBottomSkyline[col]) {
+          counter += leftRightSkyline[row] - grid[row][col];
+          grid[row][col] = leftRightSkyline[row];
+        } else {
+          counter += topBottomSkyline[col] - grid[row][col];
+          grid[row][col] = topBottomSkyline[col];
+        }
+      }
+    }
+    return counter;
+};
+
+maxIncreaseKeepingSkyline([
+  [52,49,25],
+  [100,56,51],
+  [62,55,80]
+])
