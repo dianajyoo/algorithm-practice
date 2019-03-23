d00c8bcebd5477824bfcac48200adae00fa84863
dianajyoo <diana.yooj@gmail.com>
2019-03-23 15:42:23 -0700

    edited isAnagram.js

--- leetcode/e-isAnagram.js
+++ leetcode/e-isAnagram.js
@@ -18,6 +18,12 @@ var isAnagram = function(s, t) {
 // if you cannot use sort() --> but slower runtime:
 
 var isAnagram = function(s, t) {
+    let tArr = t.split(""), sArr = s.split("")
+
+    if (s.length !== t.length) {
+      return false
+    }
+
     for (let i = 0; i < tArr.length; i++) {
       if (sArr.includes(tArr[i])) {
         sArr[sArr.indexOf(tArr[i])] = ""
