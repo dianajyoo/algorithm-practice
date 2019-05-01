36297c8463ebaa245f6448bc786b5408af7fdea3
dianajyoo <diana.yooj@gmail.com>
2019-05-01 12:09:35 -0400

    got rid of extra code in fibo

--- fibonacci/e-fib.js
+++ fibonacci/e-fib.js
@@ -1,16 +1,11 @@
 const fib = (N) => {
   let fib = [0,1]
   let nextNum
-  
-  if (N === 0) return 0
-  if (N === 1) return 1
 
   for (let i = 1; i < N; i++) {
     nextNum = fib[i - 1] + fib[i]
     fib.push(nextNum)
-    
-    if (fib[N]) {
-      return fib[N]
-    }
   }
+  
+  return fib[N]
 }
